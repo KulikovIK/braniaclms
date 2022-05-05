bf209fd2ee393b3b26b7e054c6ab15fd1fc67223
Kulikov I.K <ilyatrg@gmail.com>
2022-05-05 20:43:35 +0500

    lesson3 - Templates (bugs fixed)

--- braniaclms/settings.py
+++ braniaclms/settings.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'mainapp',
 ]
 
 MIDDLEWARE = [
@@ -53,7 +54,7 @@ ROOT_URLCONF = 'braniaclms.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['./mainapp/templates/'],
+        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
