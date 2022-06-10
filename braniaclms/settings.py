e08474d63c41e557661744ddbed706562379977b
Kulikov I.K <ilyatrg@gmail.com>
2022-06-10 09:59:44 +0500

    lesson9 - Prod

--- braniaclms/settings.py
+++ braniaclms/settings.py
@@ -146,7 +146,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
 if ENV_TYPE == 'local':
     # Работа со статикой локально
     STATICFILES_DIRS = [
